f73a29b4e82b1095b6fb19ff2e2b97387b89eb7b
alec <alec.hulce@gmail.com>
2014-06-27 14:45:08 -0700

    u

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 // temp hack till i can fix at domain record level
-if (isset($_GET['d'])) { var_dump($_SERVER); exit; }
+var_dump($_SERVER);exit;
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
